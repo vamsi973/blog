75b38186257bfe6a7ee362339df8dc7562a8971e
vamsikrishna <danda.vamsikrishna@gmail.com>
2023-08-20 16:47:50 +0530

    summary

--- scully.blog.config.ts
+++ scully.blog.config.ts
@@ -1,14 +1,8 @@
-
 import { ScullyConfig } from '@scullyio/scully';
+
 export const config: ScullyConfig = {
-projectRoot:'./src',
-projectName:'blog',
-outDir:'./dist/static',
-routes:{
-    '/blog/:id': {
-      type: 'contentFolder',
-      id: {
-        folder: "./blog"
-      }
-    },}
-}
+  projectRoot: './src',
+  projectName: 'blog',
+  outDir: './dist/static',
+  routes: {},
+};
