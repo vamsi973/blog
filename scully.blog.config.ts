653765b6528a8a4b26ded692fb91794470c8debd
vamsikrishna <danda.vamsikrishna@gmail.com>
2023-08-20 16:56:01 +0530

    Update scully.blog.config.ts

--- scully.blog.config.ts
+++ scully.blog.config.ts
@@ -5,4 +5,20 @@ export const config: ScullyConfig = {
   projectName: 'blog',
   outDir: './dist/static',
   routes: {},
+  puppeteerLaunchOptions: {
+    args: [
+      "--disable-gpu",
+      "--renderer",
+      "--no-sandbox",
+      "--no-service-autorun",
+      "--no-experiments",
+      "--no-default-browser-check",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions"
+    ]
+  }
 };
